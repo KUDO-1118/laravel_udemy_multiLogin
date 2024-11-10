ca9c3b6be9154f5890b813763d625254e85695a7
{KUDO} <au4saf010@gmail.com>
2024-11-10 14:19:19 +0900

    indexcreate

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -9,7 +9,13 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
-                  エロくアント
+
+                @foreach ($owners as $owner)
+                {{ $owner->name }}
+                {{ $owner->email }}
+                {{ $owner->created_at->diffForHumans() }}
+              @endforeach
+                {{-- エロくアント
                   @foreach ($e_all as $e_owner)
                     {{ $e_owner->name }}
                     {{ $e_owner->created_at->diffForHumans() }}
@@ -18,7 +24,7 @@
                   クエリビルダ
                   @foreach ($q_get as $q_owner)
                     {{ $q_owner->name }}
-                    {{ Carbon\Carbon::parse($q_owner->created_at)->diffForHumans() }}
+                    {{ Carbon\Carbon::parse($q_owner->created_at)->diffForHumans() }} --}}
                     {{-- $q_owner->created_at->diffForHumans()はカーボンでないというエラーになる --}}
 
                   @endforeach
