af95918731f2b7ade8227c91cd6a9eecea7fb0df
{KUDO} <au4saf010@gmail.com>
2025-01-22 14:29:09 +0900

    constant

--- config/app.php
+++ config/app.php
@@ -204,6 +204,7 @@ return [
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'Constant' => App\Constants\Common::class,
         'Date' => Illuminate\Support\Facades\Date::class,
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
