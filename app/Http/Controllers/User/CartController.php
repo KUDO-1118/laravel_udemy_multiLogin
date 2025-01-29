5814f0ab033292c4a0353dc69f16ca1d4ece37f9
{KUDO} <au4saf010@gmail.com>
2025-01-29 19:54:46 +0900

    cart delete

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,9 +6,27 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class CartController extends Controller
 {
+
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;//多対多のりレーション
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        // dd($products, $totalPrice);
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('user_id', Auth::id())->where('product_id', $request->product_id)->first();//カートに商品があるか確認
@@ -26,4 +44,12 @@ class CartController extends Controller
 
         return redirect()->route('user.cart.index');
     }
+
+    public function delete($id)
+    {
+        Cart::where('product_id', $id)->where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.cart.index');
+    }
+
 }
