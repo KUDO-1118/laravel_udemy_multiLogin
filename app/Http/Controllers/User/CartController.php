0bb347a2b7f5d092c6af02490dd2070d64920096
{KUDO} <au4saf010@gmail.com>
2025-01-30 17:23:12 +0900

    stripe controller

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers\User;
+require_once base_path('vendor/autoload.php');
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+use Illuminate\Support\Composer;
 
 class CartController extends Controller
 {
@@ -50,6 +54,44 @@ class CartController extends Controller
         Cart::where('product_id', $id)->where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.cart.index');
+    }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+
+        $lineItems = [];
+        foreach($products as $product){
+            $lineItem = [
+                'price_data' => [
+                    'currency' => 'jpy',
+                    'product_data' => [
+                        'name' => $product->name,
+                        'description' => $product->information,
+                    ],
+                    'unit_amount' => $product->price,
+                ],
+                'quantity' => $product->pivot->quantity
+            ];
+            array_push($lineItems, $lineItem);
+        }
+
+        // dd($lineItems);
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => $lineItems,
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index')
+        ]);
+        dd($session);
+
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+
+        return view('user.checkout', compact('session', 'publicKey'));
     }
 
 }
